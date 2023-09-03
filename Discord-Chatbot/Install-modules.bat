bba11be331e23f4c4978e04046e9299c1c59fb5d
itsananya <100284105+itsananyaa@users.noreply.github.com>
2023-09-04 00:15:34 +0530

    Add files via upload

---
+++ Discord-Chatbot/Install-modules.bat
@@ -0,0 +1,4 @@
+@echo off
+npm install discord.js
+npm install node-fetch
+console.log('Installing discord.js to project')
---
+++ Discord-Chatbot/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //CHANGE ALL OF THESE
+    channelid: "Channel ID",
+    token: "TOKEN",
+    botname: "Insert Name Here",
+
+    // API AUTHORIZATION
+    authorization: "API KEY"
+}
